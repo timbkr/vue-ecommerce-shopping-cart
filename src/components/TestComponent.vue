a365af20eece0f242e7e1f2de0a3b42aae0692f1
Tim Becker <tim.becker@mni.thm.de>
2023-02-21 07:40:07 +0100

    initial commit

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>TestComponent</p>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TestComponent from '../components/TestComponent.vue';
+
+</script>
+
+<template>
+  <main>
+    main
+    <TestComponent></TestComponent>
+  </main>
+</template>
