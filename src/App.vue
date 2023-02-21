fdc90e4acd66f9d263208925b85e9d8f9c19079b
Tim Becker <tim.becker@mni.thm.de>
2023-02-21 09:09:22 +0100

    some initial setup

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <div class="wrapper">
+      <img src="./assets/images/icon-menu.svg" alt="image of menu icon">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -13,9 +14,30 @@ import { RouterLink, RouterView } from 'vue-router'
     </div>
   </header>
 
+  Collections
+  Men
+  Women
+  About
+  Contact
+
+  Sneaker Company
+
+  Fall Limited Edition Sneakers
+
+  These low-profile sneakers are your perfect casual wear companion. Featuring a 
+  durable rubber outer sole, they’ll withstand everything the weather can offer.
+
+  $125.00
+  50%
+  $250.00
+
+  0
+  Add to cart
+  
   <RouterView />
+
+<footer><div class="ta-center">Coded by T</div></footer>
 </template>
 
 <style scoped>
-
 </style>
