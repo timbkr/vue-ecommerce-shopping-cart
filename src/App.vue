b0f3b2a7f406e05bbbb021561c3b6b66fca5c5db
Tim Becker <tim.becker@mni.thm.de>
2023-02-21 15:21:44 +0100

    add mobile menu

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue';
 
+let showMobileMenu = ref(false);
+function openMenuMobile() {
+  console.log("Open NavMenu");
+  showMobileMenu.value = true;
+}
+function closeMenuMobile(){
+  showMobileMenu.value = false;
+}
 </script>
 
 <template>
   <header>
     <div class="container row header">
       <div class="left row">
-        <img id="menuIconImg" src="./assets/images/icon-menu.svg" alt="image of menu icon">
+        <img @click="openMenuMobile" id="menuIconImg" src="./assets/images/icon-menu.svg" alt="image of menu icon">
         <img src="./assets/images/logo.svg" alt="image of sneakers brand logo">
       </div>
       <div class="right row">
@@ -17,43 +26,75 @@ import { RouterLink, RouterView } from 'vue-router'
     </div>
   </header>
 
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/collections">Collections</RouterLink>
-    <RouterLink to="/men">Men</RouterLink>
-    <RouterLink to="/women">Women</RouterLink>
-    <RouterLink to="/contact">Contact</RouterLink>
-  </nav>
+  <div v-if="showMobileMenu" class="mobileMenu">
+    <div class=" row header">
+      <div class="left row">
+        <img @click="closeMenuMobile"  src="./assets/images/icon-close.svg" alt="image of menu icon">
+      </div>
+    </div>
+    <nav class="bold">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/collections">Collections</RouterLink>
+      <RouterLink to="/men">Men</RouterLink>
+      <RouterLink to="/women">Women</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/contact">Contact</RouterLink>
+    </nav>
+  </div>
 
   <RouterView />
 
 
-<footer><div class="ta-center">Coded by T</div></footer>
+  <footer>
+    <div class="ta-center">Coded by T</div>
+  </footer>
 </template>
 
 <style scoped>
-.header{
+.mobileMenu {
+  position: fixed;
+  width: 65vw;
+  height: 100vh;
+  z-index: 500;
+  background-color: white;
+  padding-left: 5vw;
+}
+
+header {
+  background-color: var(--color-bg);
+}
+
+.header {
   justify-content: space-between;
   align-items: center;
   padding: 2.5vh 0;
 }
-.header img{
+
+.header img {
   height: 2.7vh;
   object-fit: contain;
   cursor: pointer;
 }
 
-.left, .right{
+.left,
+.right {
   gap: 4.2vw;
 }
 
-#menuIconImg{
+#menuIconImg {
   height: 2.2vh;
   align-self: flex-end;
 }
 
-nav{
-  display: none;
+nav {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5em;
+  padding-top: 1.5em ;
+}
+nav a{
+  text-decoration: none;
+  color: var(--color-text);
+  font-size: 1.4rem;
 }
 </style>
--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -2,40 +2,37 @@
 import { ref } from 'vue'
 let amount = ref(0);
 
-function increase(){
+function increase() {
     amount.value++;
 }
-function decrease(){
+function decrease() {
     amount.value--;
 }
 
 
-// const imageUrl = new URL('./logo.png', import.meta.url).href
-let images = ref([
-//  new URL('../assets/images/image-product-1.jpg', import.meta.url).href,
-//  new URL('../assets/images/image-product-2.jpg', import.meta.url).href,
-//  new URL('../assets/images/image-product-3.jpg', import.meta.url).href,
-//  new URL('../assets/images/image-product-4.jpg', import.meta.url).href,
-    	            '../assets/images/image-product-1.jpg',
-    	            '../assets/images/image-product-2.jpg',
-                    '../assets/images/image-product-3.jpg',
-                    '../assets/images/image-product-4.jpg',
-                ])
+//Infos: https://stackoverflow.com/questions/66419471/vue-3-vite-dynamic-image-src
+let images = ref([ //wenn imgs nicht in public ordner (nur in src/assets dann:)
+    //  new URL('../assets/images/image-product-1.jpg', import.meta.url).href,
+    '../assets/images/image-product-1.jpg',
+    '../assets/images/image-product-2.jpg',
+    '../assets/images/image-product-3.jpg',
+    '../assets/images/image-product-4.jpg',
+])
 
 let currentImgIndex = ref(0)
 let currentImg = ref(images.value[currentImgIndex.value])
 
-function nextPicture(){    
+function nextPicture() {
     currentImgIndex.value++;
-    if(currentImgIndex.value >= images.value.length){
+    if (currentImgIndex.value >= images.value.length) {
         currentImgIndex.value = 0;
     }
     currentImg.value = images.value[currentImgIndex.value]
 }
 
-function previousPicture(){
+function previousPicture() {
     currentImgIndex.value--;
-    if(currentImgIndex.value < 0) currentImgIndex.value = images.value.length-1;
+    if (currentImgIndex.value < 0) currentImgIndex.value = images.value.length - 1;
     currentImg.value = images.value[currentImgIndex.value]
 
 }
@@ -59,7 +56,7 @@ function previousPicture(){
         <h1>Fall Limited Edition Sneaker</h1>
         <p class="description">These low-profile sneakers are your perfect casual wear companion. Featuring a
             durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
-    
+
         <div class="row priceRow">
             <div class="left row">
                 <div class="price bold">$125.00</div>
@@ -67,24 +64,26 @@ function previousPicture(){
             </div>
             <div class="oldPrice bold">$250.00</div>
         </div>
-    
+
         <div class="row amount">
             <div class="amountOuter" @click="decrease">
-                <img  src="../assets/images/icon-minus.svg" alt="minus icon to decrease product amount">
+                <img src="../assets/images/icon-minus.svg" alt="minus icon to decrease product amount">
             </div>
             <input class="bold" type="number" name="" id="" v-model="amount">
             <div class="amountOuter amountRight" @click="increase">
                 <img src="../assets/images/icon-plus.svg" alt="plus icon to increase product amount">
             </div>
         </div>
-    
-        <button class="addToCartBTN"><img src="../assets/images/icon-cart-white.svg" alt="shopping cart icon"> Add to cart</button>
+
+        <button class="addToCartBTN"><img src="../assets/images/icon-cart-white.svg" alt="shopping cart icon"> Add to
+            cart</button>
     </div>
 </template>
 
 <style scoped>
 /** Image Buttons */
-.previous, .next{
+.previous,
+.next {
     position: absolute;
     top: 45%;
     z-index: 5;
@@ -95,58 +94,71 @@ function previousPicture(){
     padding: 1em;
     border-radius: 50%;
 }
-.previous{
+
+.previous {
     left: 5vw;
 }
-.next{
+
+.next {
     right: 5vw;
 }
 
-img{
+.productImages {
+    max-height: 37vh;
+    overflow: hidden;
+}
+
+img {
     object-fit: contain;
 }
 
 /** Styles */
-h1{
+h1 {
     padding: 0.5em 0;
     /* line-height: 1; */
 }
-h2{
+
+h2 {
     color: var(--color-primary);
     font-size: 1rem;
     letter-spacing: 0.1rem;
     padding-top: 1.35em;
 }
-p{
+
+p {
     color: var(--color-text-sec)
 }
 
-.priceRow{
+.priceRow {
     font-size: 1.2rem;
     align-items: center;
     justify-content: space-between;
     padding: 0.8em 0;
 }
-.left{
+
+.left {
     align-items: center;
     gap: 1em
 }
-.price{
+
+.price {
     font-size: 2rem;
 }
-.rabatt{
+
+.rabatt {
     color: var(--color-primary);
     background-color: var(--color-elements);
     border-radius: 5px;
     padding: 0.15em 0.4em;
 
 }
-.oldPrice{
+
+.oldPrice {
     color: var(--Grayish-blue);
     text-decoration: line-through;
 }
 
-.amount{
+.amount {
     /* width: 100%; */
     background-color: var(--Light-grayish-blue);
     padding: 0.8em 1.5em;
@@ -154,22 +166,26 @@ p{
     justify-content: space-between;
     margin-bottom: 1em;
 }
-.amountOuter{
+
+.amountOuter {
     flex: 1;
     display: flex;
     align-items: center;
 }
-.amountRight{
+
+.amountRight {
     justify-content: flex-end
 }
-.amount input{
+
+.amount input {
     text-align: center;
     background-color: var(--Light-grayish-blue);
     border: none;
     width: 25%;
     padding: 0.5em 0;
 }
-.addToCartBTN{
+
+.addToCartBTN {
     background-color: var(--color-primary);
     border: none;
     border-radius: 10px;
@@ -179,6 +195,6 @@ p{
     display: flex;
     justify-content: center;
     gap: 1em;
-}
-
-</style>
+    -webkit-box-shadow: 0px 19px 23px -23px #FF7D1A; 
+box-shadow: 0px 19px 23px -23px #FF7D1A;
+}</style>
