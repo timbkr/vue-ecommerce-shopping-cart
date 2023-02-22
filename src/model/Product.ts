bf42d754d5165d5da134b2033065e2d0bb40c6e2
Tim Becker <tim.becker@mni.thm.de>
2023-02-22 17:03:09 +0100

    fixes

--- src/model/Product.ts
+++ src/model/Product.ts
@@ -15,5 +15,6 @@ export default class Product{
         this.description = description;
         this.price = price;
         this.reduziert = reduziert;
+        this.oldprice = oldprice;
     }
 }
